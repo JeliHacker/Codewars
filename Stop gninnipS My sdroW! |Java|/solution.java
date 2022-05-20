41a3962b6aae49f2064355e9c70ae51c0cd66186
JeliHacker <jeligooch@gmail.com>
2022-05-19 23:01:26 -0400

    stop spinning my words (fixed)

--- Stop gninnipS My sdroW! |Java|/solution.java
+++ Stop gninnipS My sdroW! |Java|/solution.java
@@ -1,30 +1,75 @@
-import java.util.Arrays;
-
-public class SpinWords {
-
-  public String spinWords(String sentence) {
-    String[] words = sentence.split(" ");
-    for (int i=0; i<words.length; i++) {
-      if (words[i].length() >= 5) {
-        words[i] = new StringBuilder(words[i]).reverse().toString();
+public class SpinWords 
+{
+  public String spinWords(String sentence) 
+  {
+//     String testString = "Hey fellow warriors";
+  
+//     System.out.println(sentence.length());
+//     System.out.println("(0, 3) = |" + testString.substring(0, 4) + "|");
+//     System.out.println("(4, 11) = |" + testString.substring(4, 11)+ "|");
+//     System.out.println("(11, 19) = |" + testString.substring(11, 19)+ "|");
+    
+//     for(int i = 9; i >= 4; i--)
+//     {
+//       System.out.println(testString.charAt(i));
+//     }
+    
+    int letterCount = 0;
+    String newString = "";
+    int wordFirstLetter = 0;
+    String newWord = "";
+    
+    for(var j = 0; j < sentence.length(); j++)
+    {
+      letterCount++;
+      boolean spaceDetected = sentence.charAt(j) == ' ';
+      
+      //System.out.println("j = " + j);
+      System.out.println("letterCount = " + letterCount + "| sentence.charAt(" + j + ") = " + sentence.charAt(j));
+      
+      if(spaceDetected)
+      {
+        if(letterCount > 5)
+        {
+          for(int b = j-1; b >= wordFirstLetter; b--)
+          {
+            newWord += sentence.charAt(b);
+          }
+        } 
+        else
+        {
+          newWord = sentence.substring(wordFirstLetter, j);
+        }
+        
+        newString += newWord;
+        newString += " ";
+        newWord = "";
+        wordFirstLetter = j + 1;
+        letterCount = 0;
       }
+      
+      // end of string
+      if((j + 1) == sentence.length())
+      {
+        if(letterCount > 5)
+        {
+          for(int b = j; b >= wordFirstLetter; b--)
+          {
+            newWord += sentence.charAt(b);
+          }
+        } 
+        else
+        {
+          newWord = sentence.substring(wordFirstLetter, sentence.length());
+        }
+        
+        newString += newWord;
+      }
+      
     }
-    return String.join(" ",words);
-  }
-}
-6 similar code variations are grouped with this one
-Show Variations
-Best Practices547Clever215
- 20ForkCompare with your solutionLink
-SithFire, ChungGor, reiayanami, zwirsi, Alejo-alvarezv, xryzzz1bg, Sinoffate, Joseph5
-import java.util.stream.*;
-import java.util.Arrays;
-
-public class SpinWords {
-
-  public String spinWords(String sentence) {
-    return Arrays.stream(sentence.split(" "))
-                 .map(i -> i.length() > 4 ? new StringBuilder(i).reverse().toString() : i)
-                 .collect(Collectors.joining(" "));
-  }
+    
+    System.out.println("newString = " + newString);
+    return newString;
+ } 
+ 
 }
